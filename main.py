302dff21e13a14128280ec03b702bd11ffa4010c
Santhosh Thampuran <santhosh.thampuran@gmail.com>
2017-10-01 15:36:57 -0400

    setup logger partially

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import logger
+import logging
 
-log.setlevel("DEBUG")
+logger = logging.getLogger("main.py")
+logging.basicConfig()
+
+
+# fh = logging.FileHandler("log.txt")
+# formatter = logging.Formatter("%(asctime)s - %(name)s - %(level)s - \
+#    %(message)s")
+# fh.setFormatter(formatter)
+# logger.addHandler(fh)
+
+
+logger.setLevel(logging.DEBUG)
+
+logger.info("this is a test message %s %s", "yes", "no")
